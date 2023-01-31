e7c7d29401be8bdde24fb6d06507cbd1985466e9
atabong <tajoacha@gmail.com>
2023-01-31 13:04:56 +0300

    Hashtable devs

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane","Jones",123);
+        Employee johnDoe = new Employee("John","Doe",4567);
+        Employee marySmith = new Employee("Mary","Smith",22);
+        Employee mikeWilson = new Employee("Mike","Wilson",3245);
+        Employee billEnd = new Employee("Bill","End",78);
+
+        SimpleHashtable ht = new SimpleHashtable();
+
+        ht.put("Jones",janeJones);
+        ht.put("Doe",johnDoe);
+        ht.put("Wilson",mikeWilson);
+        ht.put("Smith",marySmith);
+        ht.put("End",billEnd);
+
+         ht.printHashtable();
+        System.out.println();
+        System.out.println("Retrieve key wilson: "+ht.get("Wilson"));
+        ht.remove("Wilson");
+        ht.remove("Jones");
+        ht.printHashtable();
+
+
+
+    }
+}
